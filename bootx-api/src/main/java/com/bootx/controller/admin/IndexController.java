23a15a661cea221374b3254b010c057186664161
1169794338@qq.com <1169794338@qq.com>
2021-04-01 22:30:41 +0800

    init

--- bootx-api/src/main/java/com/bootx/controller/admin/IndexController.java
+++ bootx-api/src/main/java/com/bootx/controller/admin/IndexController.java
@@ -1,18 +1,12 @@
 package com.bootx.controller.admin;
 
-import com.bootx.StsServiceSample;
 import com.bootx.common.Result;
 import com.bootx.controller.BaseController;
 import com.bootx.entity.Admin;
 import com.bootx.entity.App;
-import com.bootx.entity.BaseEntity;
-import com.bootx.member.entity.Member;
 import com.bootx.service.AdminService;
 import com.bootx.service.AppService;
-import com.bootx.util.CodeUtils;
-import com.bootx.util.DateUtils;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
@@ -89,23 +83,4 @@ public class IndexController extends BaseController {
         appService.update(app);
         return Result.success(data);
     }
-
-
-    @PostMapping("/download")
-    private Result download(HttpServletRequest request) {
-        App app = appService.get1(request);
-        Map<String,Object> data = new HashMap<>();
-        if(app==null){
-            return Result.error("非法访问");
-        }
-        if(app.getType()==1){
-            data.put("code",0);
-            data.put("downloadUrl",StsServiceSample.getUrl("movie.zip"));
-        }else{
-            data.put("code",-1);
-            data.put("msg","暂无该小程序源码");
-        }
-        return Result.success(data);
-    }
-
 }
--- bootx-api/src/main/java/com/bootx/entity/Novel.java
+++
@@ -1,2 +0,0 @@
-package com.bootx.entity;public class Novel {
-}
