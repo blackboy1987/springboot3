f456937df8766ab162c04ceb034e3eda0653b20f
1169794338@qq.com <1169794338@qq.com>
2021-04-08 19:09:56 +0800

    init

--- bootx-music/src/main/java/com/bootx/controller/DownloadController.java
+++ bootx-music/src/main/java/com/bootx/controller/DownloadController.java
@@ -21,6 +21,7 @@ import javax.annotation.Resource;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -36,33 +37,58 @@ public class DownloadController {
 
     @Resource
     private RedisService redisService;
+    @Resource
+    private JdbcTemplate jdbcTemplate;
 
     @GetMapping
-    public Result index(Long id) {
-        for (Long start = id;start<(id+10);start++) {
-            Novel novel = novelService.find(start);
-            boolean flag = true;
-            if(novel==null){
-                continue;
-            }
-            File file = new File(id+"_"+novel.getTitle());
-            System.out.println(file.exists());
-            for (NovelItem novelItem:novel.getNovelItems()){
-                if(!flag){
-                    break;
+    public Result index() {
+        ExecutorService fixedThreadPool = Executors.newFixedThreadPool(10);
+        List<Map<String,Object>> list = jdbcTemplate.queryForList("select id,itemCount,url from novel order by itemCount asc, id asc");
+        for (int i=0;i<list.size();i++) {
+            int finalI = i;
+            fixedThreadPool.execute(()->{
+                Novel novel = novelService.find(Long.valueOf(list.get(finalI).get("id")+""));
+                if(novel!=null){
+                    File file = new File("/Volumes/blackboy/有声小说/"+novel.getCategoryName()+"/"+parse(novel.getId())+"_"+novel.getTitle());
+                    if(!file.exists()){
+                        for (NovelItem novelItem:novel.getNovelItems()){
+                            String resourceUrl = novelItem.getResourceUrl();
+                            System.out.println(novel.getId()+":"+resourceUrl);
+                            String path = "/Volumes/blackboy/有声小说/"+novel.getCategoryName()+"/"+parse(novel.getId())+"_"+novel.getTitle()+"_"+novel.getMemo()+"/"+novelItem.getOrder()+"_"+novelItem.getTitle()+"."+ FilenameUtils.getExtension(resourceUrl);
+                            try {
+                                DownloadUtils.download(resourceUrl,path);
+                            }catch (Exception e){
+                                e.printStackTrace();
+                                FileUtils.deleteQuietly(new File(path));
+                            }
+                        }
+                    }else{
+                        System.out.println(novel.getId()+":已存在！！！");
+                    }
                 }
-                String resourceUrl = novelItem.getResourceUrl();
-                System.out.println(novel.getId()+":"+resourceUrl);
-                String path = "D:/有声小说/"+novel.getCategoryName()+"/"+novel.getId()+"_"+novel.getTitle()+"_"+novel.getMemo()+"/"+novelItem.getOrder()+"_"+novelItem.getTitle()+"."+ FilenameUtils.getExtension(resourceUrl);
-                try {
-                    DownloadUtils.download(resourceUrl,path);
-                }catch (Exception e){
-                    e.printStackTrace();
-                    FileUtils.deleteQuietly(new File(path));
-                    flag = false;
-                }
-            }
+            });
         }
+
+
+
+
         return Result.success("ok");
+    }
+
+    private String parse(Long id) {
+        if(id<10){
+            return "0000"+id;
+        }
+        if(id<100){
+            return "000"+id;
+        }
+        if(id<1000){
+            return "00"+id;
+        }
+        if(id<10000){
+            return "0"+id;
+        }
+        return id+"";
+
     }
 }
