8983e95992a7dc418d8981bf8b22cc78947f4851
black <1169794338@qq.com>
2024-03-13 18:15:51 +0800

    init

--- baidu_api/src/main/java/com/bootx/controller/api/IndexController.java
+++ baidu_api/src/main/java/com/bootx/controller/api/IndexController.java
@@ -196,7 +196,7 @@ public class IndexController extends BaseController {
 			return Result.success(Collections.emptyList());
 		}
 		List<Map<String, Object>> maps = jdbcTemplate.queryForList("select fsId,playUrl,path from filelist where grade=? and category=1 and parent_id=? order by orders asc",fileList.getGrade()+1,fileList.getId() );
-		if(!maps.isEmpty()){
+		if(maps.isEmpty()){
 			maps = jdbcTemplate.queryForList("select fsId,playUrl,path from filelist where grade=? and category=1 and treePath =? order by orders asc",fileList.getGrade()+2,fileList.getTreePath()+fileList.getId()+"," );
 		}
 		List<Map<String, Object>> finalMaps = maps;
